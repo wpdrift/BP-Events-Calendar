c87f7d45e9383eaeaced5bb4b14ba5ffdd35e0ea
Shamim Al Mamun <shamim.moeen@gmail.com>
2018-03-30 10:08:45 +0600

    Fixed the venue section inside the frontend add event meta box

--- includes/bpec-templates.php
+++ includes/bpec-templates.php
@@ -131,7 +131,7 @@ function bpec_currency_symbol_field() {
  */
 function bpec_get_actions_link() {
     global $bp;
-    
+
     $event_form = BPEC_Event_Form::instance();
 
     $edit_link = $view_link = '';
@@ -163,7 +163,7 @@ function bpec_get_actions_link() {
  */
 function bpec_event_title_field() {
     $event_form = BPEC_Event_Form::instance();
-    
+
     $title = get_the_title( $event_form->event_id );
     if ( empty( $title ) && ! empty( $_POST['post_title'] ) ) {
         $title = stripslashes( $_POST['post_title'] );
@@ -359,7 +359,7 @@ function bpec_template_datepickers() {
     $events_label_singular = tribe_get_event_label_singular();
     $events_label_singular_lowercase = tribe_get_event_label_singular_lowercase();
     $events_label_plural_lowercase = tribe_get_event_label_plural_lowercase();  ?>
-    
+
     <!-- Event Date Selection -->
     <?php do_action( 'tribe_events_community_before_the_datepickers' ); ?>
 
@@ -441,211 +441,32 @@ function bpec_template_datepickers() {
  */
 function bpec_template_venue() {
 
-    // We need the variables here otherwise it will throw notices
-    $venue_label_singular = tribe_get_venue_label_singular();
-    $event_form = BPEC_Event_Form::instance();
-
-    // If not $_POST get the current values to edit
-    if ( ! $_POST ) {
-        $postId              = Tribe__Events__Main::postIdHelper();
-        $venue_name          = tribe_get_venue();
-        $venue_phone         = tribe_get_phone();
-        $venue_website       = strip_tags( tribe_get_venue_website_link( null, null ) );
-        $venue_address       = tribe_get_address();
-        $venue_city          = tribe_get_city();
-        $venue_province      = tribe_get_province();
-        $venue_state         = tribe_get_state();
-        $venue_country       = tribe_get_country();
-        $venue_zip           = tribe_get_zip();
-        $venue_show_map_link = get_post_meta( $postId, '_EventShowMapLink', true );
-        $venue_show_map      = tribe_embed_google_map( $postId );
-
-        //If we just saved use those values from $_POST
-    } elseif ( ! empty( $_POST ) ) {
-        $venue_name          = isset( $_POST['venue']['Venue'] ) ? esc_attr( $_POST['venue']['Venue'] ) : '';
-        $venue_phone         = isset( $_POST['venue']['Phone'] ) ? esc_attr( $_POST['venue']['Phone'] ) : '';
-        $venue_website       = isset( $_POST['venue']['URL'] ) ? esc_attr( $_POST['venue']['URL'] ) : '';
-        $venue_address       = isset( $_POST['venue']['Address'] ) ? esc_attr( $_POST['venue']['Address'] ) : '';
-        $venue_city          = isset( $_POST['venue']['City'] ) ? esc_attr( $_POST['venue']['City'] ) : '';
-        $venue_province      = isset( $_POST['venue']['Province'] ) ? esc_attr( $_POST['venue']['Province'] ) : '';
-        $venue_state         = isset( $_POST['venue']['State'] ) ? esc_attr( $_POST['venue']['State'] ) : '';
-        $venue_country       = isset( $_POST['venue']['Country'] ) ? esc_attr( $_POST['venue']['Country'] ) : '';
-        $venue_zip           = isset( $_POST['venue']['Zip'] ) ? esc_attr( $_POST['venue']['Zip'] ) : '';
-        $venue_show_map_link = isset( $_POST['EventShowMapLink'] ) ? esc_attr( $_POST['EventShowMapLink'] ) : '';
-        $venue_show_map      = isset( $_POST['EventShowMap'] ) ? esc_attr( $_POST['EventShowMap'] ) : '';
-    }
-
     if ( ! isset( $event ) ) {
-        $event = $event_form->event;
-    }
-?>
+        $event = Tribe__Events__Main::postIdHelper();
+    } ?>
 
-<!-- Venue -->
-<div class="buddypress-events-calendar-details eventForm bubble" id="event_tribe_venue">
+    <!-- Venue -->
+    <div class="buddypress-events-calendar-details eventForm bubble" id="event_tribe_venue">
 
-    <table class="tribe-community-event-info" cellspacing="0" cellpadding="0">
-        <tbody>
-        <tr>
-            <td colspan="2" class="tribe_sectionheader">
-                <h4><label class=""><?php
-                        printf( __( '%s Details', 'bp-events-calendar' ), $venue_label_singular );
-                        //	echo bpec_field_marker( 'venue' );
-                        ?></label></h4>
-            </td><!-- .tribe_sectionheader -->
-        </tr>
+        <table class="tribe-community-event-info" cellspacing="0" cellpadding="0">
 
-        <?php  do_action( 'tribe_venue_table_top', $event_form->event_id ); ?>
+            <thead> <tr>
+                <td colspan="2" class="tribe_sectionheader">
+                    <h4> <label class=""> <?php
+                            printf( __( '%s Details', 'bp-events-calendar' ), tribe_get_venue_label_singular() );
+                            ?> </label> </h4>
+                </td><!-- .tribe_sectionheader -->
+            </tr> </thead>
 
-        <?php if ( ! bpec_is_venue_edit_screen() ) { ?>
-            <tr class="venue linked-post">
-                <td>
-                    <label for="VenueVenue" <?php if ( $event && $_POST && empty( $venue_name ) ) echo 'class="error"'; ?>>
-                        <?php printf( __( '%s Name', 'bp-events-calendar' ), $venue_label_singular ); ?>:
-                    </label>
-                </td>
-                <td>
-                    <input type="text" id="VenueVenue" name="venue[Venue]" size="25"  value="<?php esc_attr_e( $venue_name ); ?>" />
-                </td>
-            </tr><!-- .venue -->
-        <?php } ?>
-
-        <tr class="venue linked-post">
-            <td>
-                <label for="VenueAddress">
-                    <?php esc_html_e( 'Address', 'bp-events-calendar' ); ?>:
-                </label>
-            </td>
-            <td>
-                <input type="text" id="VenueAddress" name="venue[Address]" size="25" value="<?php esc_attr_e( $venue_address ); ?>" />
-            </td>
-        </tr><!-- .venue -->
-
-        <tr class="venue linked-post">
-            <td>
-                <label for="VenueCity">
-                    <?php esc_html_e( 'City', 'bp-events-calendar' ); ?>:
-                </label>
-            </td>
-            <td><input type="text" id="VenueCity" name="venue[City]" size="25" value="<?php esc_attr_e( $venue_city ); ?>" /></td>
-        </tr><!-- .venue -->
-
-
-        <tr class="venue linked-post">
-            <td>
-                <label for="EventCountry">
-                    <?php esc_html_e( 'Country', 'bp-events-calendar' ); ?>:
-                </label>
-            </td>
-            <td>
-                <select class="chosen" name="venue[Country]" id="EventCountry">
-                    <?php
-                    foreach ( Tribe__View_Helpers::constructCountries() as $abbr => $fullname ) {
-                        echo '<option value="'. esc_attr( $fullname ) .'" ';
-
-                        selected( $venue_country == $fullname );
-                        echo '>' . esc_html( $fullname ) . '</option>';
-                    } ?>
-                </select>
-            </td>
-        </tr><!-- .venue -->
-
-
-
-        <tr class="venue linked-post">
             <?php
-            if ( ! isset( $venue_stateProvince ) || $venue_stateProvince == '' ) {
-                $venue_stateProvince = -1;
-            }
+            // The venue meta box will render everything within a <tbody>
+            $venue_meta_box = new Tribe__Events__Linked_Posts__Chooser_Meta_Box( $event, Tribe__Events__Venue::POSTTYPE );
+            $venue_meta_box->render();
             ?>
-            <td>
-                <label for="StateProvinceText">
-                    <?php esc_html_e( 'State or Province', 'bp-events-calendar' ); ?>:
-                </label>
-            </td>
-            <td>
-                <input id="StateProvinceText" name="venue[Province]" type="text" name="" size="25" value="<?php echo esc_attr( isset( $venue_province ) && $venue_province != '' && $venue_province != -1 ? $venue_province : '' ); ?>" />
-                <select class="chosen" id="StateProvinceSelect" name="venue[State]">
-                    <option value=""><?php esc_html_e( 'Select a State', 'bp-events-calendar' ); ?></option>
-                    <?php foreach ( Tribe__View_Helpers::loadStates() as $abbr => $fullname ) {
-                        echo '<option value="' . esc_attr( $abbr ) .'" ';
-                        selected( $venue_state == $abbr );
-                        echo '>'. esc_html( $fullname ) .'</option>'. "\n";
-                    } ?>
-                </select>
-            </td>
-        </tr><!-- .venue -->
 
+        </table> <!-- #event_venue -->
 
-        <tr class="venue linked-post">
-            <td>
-                <label for="EventZip">
-                    <?php esc_html_e( 'Postal Code', 'bp-events-calendar' ); ?>:
-                </label>
-            </td>
-            <td>
-                <input type="text" id="EventZip" name="venue[Zip]" size="6" value="<?php esc_attr_e( $venue_zip ); ?>" />
-            </td>
-        </tr><!-- .venue -->
-
-        <tr class="venue linked-post">
-            <td>
-                <label for="EventPhone">
-                    <?php esc_html_e( 'Phone', 'bp-events-calendar' ); ?>:
-                </label>
-            </td>
-            <td>
-                <input type="text" id="EventPhone" name="venue[Phone]" size="14" value="<?php esc_attr_e( $venue_phone ); ?>" />
-            </td>
-        </tr><!-- .venue -->
-
-        <tr class="venue linked-post">
-            <td>
-                <label for="EventWebsite">
-                    <?php esc_html_e( 'Website', 'bp-events-calendar' ); ?>:
-                </label>
-            </td>
-            <td>
-                <input type="text" id="EventWebsite" name="venue[URL]" size="14" value="<?php esc_attr_e( $venue_website ); ?>" />
-            </td>
-        </tr><!-- .venue -->
-
-        <?php
-        if ( ! bpec_is_venue_edit_screen() ) {
-            if ( tribe_get_option( 'embedGoogleMaps', true ) ) {
-                ?>
-
-                <tr id="google_map_toggle" class="remain-visible">
-                    <td>
-                        <label for="EventShowMap">
-                            <?php esc_html_e( 'Show Google Map', 'bp-events-calendar' ); ?>:
-                        </label>
-                    </td>
-                    <td>
-                        <input type="checkbox" id="EventShowMap" name="EventShowMap" value="1" <?php checked( $venue_show_map ); ?> />
-                    </td>
-                </tr><!-- #google_map_toggle -->
-                <?php
-            }
-            ?>
-            <tr id="google_map_link_toggle" class="remain-visible">
-                <td>
-                    <label for="EventShowMapLink">
-                        <?php esc_html_e( 'Show Google Maps Link', 'bp-events-calendar' ); ?>:
-                    </label>
-                </td>
-                <td>
-                    <input type="checkbox" id="EventShowMapLink" name="EventShowMapLink" value="1" <?php checked( $venue_show_map_link ); ?> />
-                </td>
-            </tr><!-- #google_map_link_toggle -->
-            <?php
-
-        } // if ( bpec_is_venue_edit_screen() )
-
-        ?>
-        </tbody>
-    </table><!-- #event_venue -->
-
-</div><?php
+    </div><?php
 
 }
 
